2f5171e830195a6b4829911e0258ba126122aa5d
Santiago Fuentes <sj.fuentes@uninandes.edu.co>
2025-04-25 11:17:02 -0500

    añadir calculo de cafes de origen y blended y parrafo de contacto

--- src/app/cafe/cafe.component.ts
+++ src/app/cafe/cafe.component.ts
@@ -13,10 +13,27 @@ export class CafeComponent {
   constructor(private cafeService: CafeService){}
 
   cafes: Array<Cafe> = [];
+  cafeOrigen: number = 0;
+  cafeBlend: number = 0;
 
   getCafesList(){
     this.cafeService.getCafes().subscribe(cafes=>{
       this.cafes = cafes
+      this.calculateCounts()
+    })
+  }
+
+  calculateCounts():void{
+    this.cafeOrigen = 0;
+    this.cafeBlend = 0;
+    
+    this.getCafesList();
+    this.cafes.forEach(cafe =>{
+      if(cafe.tipo == "Café de Origen"){
+        this.cafeOrigen++
+      }else if (cafe.tipo == "Blend"){
+        this.cafeBlend++
+      }
     })
   }
 
